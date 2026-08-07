293aa119b8a2072906b7baf3dd092cc55353184c
o1iviachen <olivia63chen@gmail.com>
2026-08-07 15:59:55 -0400

    Remove dark mode

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
 (() => {
-  const toggle = document.querySelector('.theme-toggle');
-  if (toggle) {
-    toggle.addEventListener('click', () => {
-      const next = document.documentElement.dataset.theme === 'dark' ? 'light' : 'dark';
-      document.documentElement.dataset.theme = next;
-      localStorage.setItem('theme', next);
-    });
-  }
-
   document.addEventListener('click', (e) => {
     const a = e.target.closest('a[data-track]');
     if (!a) return;
